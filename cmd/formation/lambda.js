d94cc56154af4ad9568f9a6a08eb3dd9270e71f0
Robert Coleman <github@robert.net.nz>
2015-08-02 14:19:40 +1200

    Lambda Bridge support SNS notifications

--- cmd/formation/lambda.js
+++ cmd/formation/lambda.js
@@ -7,6 +7,16 @@ exports.external = function(event, context) {
     return context.done(err);
   });
 
+  // support events via SNS
+  if (event.Records) {
+    if (event.Records[0].EventSource === 'aws:sns') {
+      event = JSON.parse(event.Records[0].Sns.Message);
+      console.log('SNS event', event);
+    } else {
+      return context.done(new Error("Unsupported event source: " + event.Records[0].EventSource));
+    }
+  }
+
   var child = require('child_process').spawn('./formation', [JSON.stringify(event)], { stdio:'inherit' });
 
   child.on('close', function(code) {
