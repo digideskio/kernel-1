a7ccf802db35c05c767fc42874d6b43a4e5044a4
Robert Coleman <github@robert.net.nz>
2015-09-03 10:33:03 +1200

    add deprecation message to Custom::EC2AvailabilityZones

--- cmd/formation/handler/ec2-availability-zones.go
+++ cmd/formation/handler/ec2-availability-zones.go
@@ -2,11 +2,6 @@ package handler
 
 import (
 	"fmt"
-	"strconv"
-	"strings"
-
-	"github.com/convox/kernel/Godeps/_workspace/src/github.com/awslabs/aws-sdk-go/aws"
-	"github.com/convox/kernel/Godeps/_workspace/src/github.com/awslabs/aws-sdk-go/service/ec2"
 )
 
 func HandleEC2AvailabilityZones(req Request) (string, map[string]interface{}, error) {
@@ -31,26 +26,7 @@ func HandleEC2AvailabilityZones(req Request) (string, map[string]interface{}, er
 }
 
 func EC2AvailabilityZonesCreate(req Request) (string, map[string]interface{}, error) {
-	_, err := EC2(req).CreateSubnet(&ec2.CreateSubnetInput{
-		AvailabilityZone: aws.String("garbage"),
-		CIDRBlock:        aws.String("10.200.0.0/16"),
-		VPCID:            aws.String(req.ResourceProperties["Vpc"].(string)),
-	})
-
-	matches := regexMatchAvailabilityZones.FindStringSubmatch(err.Error())
-	matches = strings.Split(strings.Replace(matches[1], " ", "", -1), ",")
-
-	if len(matches) < 1 {
-		return "", nil, fmt.Errorf("could not discover availability zones")
-	}
-
-	outputs := make(map[string]interface{})
-	for i, az := range matches {
-		outputs["AvailabilityZone"+strconv.Itoa(i)] = az
-	}
-
-	physical := strings.Join(matches, ",")
-	return physical, outputs, nil
+	return req.PhysicalResourceId, nil, fmt.Errorf("Custom::EC2AvailabilityZones is deprecated")
 }
 
 func EC2AvailabilityZonesUpdate(req Request) (string, map[string]interface{}, error) {
